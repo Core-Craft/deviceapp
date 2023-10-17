94589d68b17bc7325535c11a0d2e3581d9978e55
amit das <mramitdas99@gmail.com>
2023-10-17 12:27:08 +0530

    CHORE: ui -> updated file index

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,5 +1,15 @@
-import Welcome from "./welcome";
+import Welcome1 from "./welcome/welcome1";
+import Welcome2 from "./welcome/welcome2";
+import Welcome3 from "./welcome/welcome3";
+import Welcome4 from "./welcome/welcome4";
+import Welcome5 from "./welcome/welcome5";
+
+
 
 export {
-    Welcome
+    Welcome1,
+    Welcome2,
+    Welcome3,
+    Welcome4,
+    Welcome5
 }
